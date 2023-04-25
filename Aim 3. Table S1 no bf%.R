1b9a3e7463e897ac698da709657e5a602b1cdb9b
jenwang2000 <125165726+jenwang2000@users.noreply.github.com>
2023-04-25 01:38:17 -0700

    Update and rename aim 2 no bf%.R to Aim 3. Table S1 no bf%.R
    cleaned up code

--- Aim 3. Table S1 no bf%.R
+++ Aim 3. Table S1 no bf%.R
@@ -17,7 +17,7 @@ meta <- read_tsv("colombia_metadata.txt")
 tax <- read_tsv("taxonomy.tsv")
 phylotree <- read.tree("tree.nwk")
 
-# removing redundant columns
+# filtering out body fat percentage from metadata
 select <- dplyr::select
 meta_select <- meta |>
   select(-country, -Body_Fat_Percentage)
@@ -94,5 +94,3 @@ AIC(model_full) # full model (all predictors)
 AIC(stepaic_results_nobfp) # best fit model AIC
 
 capture.output(alpha_model_nobfp, file = "stepaic_alpha_nobfp.txt")
-
-capture.output(alpha_model_noins_ormeds, file = "stepaic_alpha_noinsulin_ormeds.txt")
